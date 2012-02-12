0fc0ce3f1e749f6530e473b21b57a918185b184a
Marke Hallowell <indiefan@gmail.com>
2012-02-12 00:29:09 -0800

    Refactored installation process to be a little cleaner.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ function die()
 }
 
 # Add <strong>.old</strong> to any existing Janus Customization file file in the home directory
-for i in $HOME/.janus $HOME/.vimrc.after $HOME/.gvimrc.after; do
+for i in $HOME/.janus-customizations $HOME/.janus $HOME/.vimrc.after $HOME/.gvimrc.after; do
 	if [[ ( -e $i ) || ( -h $i ) ]]; then
 		echo "${i} has been renamed to ${i}.old"
 		mv "${i}" "${i}.old" || die "Could not move ${i} to ${i}.old"
@@ -13,9 +13,10 @@ for i in $HOME/.janus $HOME/.vimrc.after $HOME/.gvimrc.after; do
 done
 
 # Clone Janus-Customizations into .janus
-git clone https://github.com/indiefan/Janus-Customizations.git $HOME/.janus \
-	|| die "Could not clone the repository to ${HOME}/.janus"
+git clone https://github.com/indiefan/Janus-Customizations.git $HOME/.janus-customizations \
+	|| die "Could not clone the repository to ${HOME}/.janus-customizations"
 
 # Create symbolic links to local config files
-ln -s $HOME/.janus/.gvimrc.after $HOME/.gvimrc.after
-ln -s $HOME/.janus/.vimrc.after $HOME/.vimrc.after
+ln -s $HOME/.janus-customizations/.janus $HOME/.janus
+ln -s $HOME/.janus-customizations/.gvimrc.after $HOME/.gvimrc.after
+ln -s $HOME/.janus-customizations/.vimrc.after $HOME/.vimrc.after
