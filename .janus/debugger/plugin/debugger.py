a128fec8eac79307891682e451249fe0371ea29f
indiefan <indiefan@gmail.com>
2012-02-20 15:54:08 -0800

    Changes to debugger plugin to avoid prompt.

--- .janus/debugger/plugin/debugger.py
+++ .janus/debugger/plugin/debugger.py
@@ -449,7 +449,7 @@ class DebugUI:
     if self.minibufexpl == 1:
       vim.command('CMiniBufExplorer')         # close minibufexplorer if it is open
     # save session
-    vim.command('mksession! ' + self.sessfile)
+    vim.command('SaveSession!')
     for i in range(1, len(vim.windows)+1):
       vim.command(str(i)+'wincmd w')
       self.winbuf[i] = vim.eval('bufnr("%")') # save buffer number, mksession does not do job perfectly
@@ -477,8 +477,8 @@ class DebugUI:
     self.destroy()
 
     # restore session
-    vim.command('source ' + self.sessfile)
-    os.system('rm -f ' + self.sessfile)
+    vim.command('OpenSession')
+    #vim.command('DeleteSession ' + self.sessfile)
 
     self.set_highlight()
 
